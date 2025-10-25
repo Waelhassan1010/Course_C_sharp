444732270db093f6e7516bf8249ce3a3bb4baca3
Waelhassan1010 <wael_hassan_mec@hotmail.com>
2025-10-26 01:33:33 +0200

    encapsulation work

--- 16.CreatingOurFirstForm-master/CookBook/UI/IngredientsForm.cs
+++ 16.CreatingOurFirstForm-master/CookBook/UI/IngredientsForm.cs
@@ -20,19 +20,30 @@ namespace CookBook.UI
 
         private void AddTofridgeBtn_Click(object sender, EventArgs e)
         {
-            //string message = NametextBox.Text
-            //    + "\n " + TypeTextBox.Text
-            //    + "\n " + WeightNum.Value
-            //    + "\n " + KcalPer100gNum.Value
-            //    + "\n " + PricePer100gNum.Value;
-
+            Ingredient ingredient = new Ingredient
+            {
+                Name = NametextBox.Text,
+                Type = TypeTextBox.Text,
+                Weight = WeightNum.Value,
+                KcalPer100g = KcalPer100gNum.Value,
+                PricePer100g = PricePer100gNum.Value
+            };
             string message= $@"
-             {NametextBox.Text}
-             {TypeTextBox.Text}
-             {WeightNum.Value}
-             {KcalPer100gNum.Value}
-             {PricePer100gNum.Value}";
+             {ingredient.Name}
+             {ingredient.Type}
+             {ingredient.Weight}
+             {ingredient.PricePer100g}
+             {ingredient.KcalPer100g}";
             MessageBox.Show(message);
         }
     }
+}
+public class Ingredient
+{ 
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public decimal Weight { get; set; }
+    public decimal KcalPer100g { get; set; }
+    public decimal PricePer100g { get; set; }
+
 }
