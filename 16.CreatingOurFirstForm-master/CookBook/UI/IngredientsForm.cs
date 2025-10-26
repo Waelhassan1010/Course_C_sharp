dbbc4a3e424da08aa3ad081f6772540d5b8d5bf5
Waelhassan1010 <wael_hassan_mec@hotmail.com>
2025-10-26 17:42:30 +0100

    add for the code how we can use Lambda EX

--- 16.CreatingOurFirstForm-master/CookBook/UI/IngredientsForm.cs
+++ 16.CreatingOurFirstForm-master/CookBook/UI/IngredientsForm.cs
@@ -20,26 +20,31 @@ namespace CookBook.UI
 
         private void AddTofridgeBtn_Click(object sender, EventArgs e)
         {
-            Ingredient ingredient = new Ingredient
-            {
-                Name = NametextBox.Text,
-                Type = TypeTextBox.Text,
-                Weight = WeightNum.Value,
-                KcalPer100g = KcalPer100gNum.Value,
-                PricePer100g = PricePer100gNum.Value
-            };
-            string message= $@"
-             {ingredient.Name}
-             {ingredient.Type}
-             {ingredient.Weight}
-             {ingredient.PricePer100g}
-             {ingredient.KcalPer100g}";
+            List<Ingredient> ingredients = new List<Ingredient>();
+            Ingredient i1 = new Ingredient { Name = "apple", Type = "Fruit", Weight = 100, KcalPer100g = 324, PricePer100g = 312 };
+            Ingredient i2 = new Ingredient { Name = "bananan", Type = "Fruit", Weight = 150, KcalPer100g = 500, PricePer100g = 312 };
+            Ingredient i3 = new Ingredient { Name = "pinaple", Type = "Fruit", Weight = 200, KcalPer100g = 400, PricePer100g = 312 };
+
+
+            ingredients.Add(i1);
+            ingredients.Add(i2);
+            ingredients.Add(i3);
+            string message = " ";
+            //foreach (Ingredient INg1 in ingredients)
+            //{
+            //    message += $@" 
+            //{INg1.Name} {INg1.Type} {INg1.Weight} {INg1.KcalPer100g} {INg1.PricePer100g}";
+
+
+            //}
+            ingredients.ForEach(INg1 => { message += $@" 
+            {INg1.Name} {INg1.Type} {INg1.Weight} {INg1.KcalPer100g} {INg1.PricePer100g}"; });
             MessageBox.Show(message);
         }
     }
 }
 public class Ingredient
-{ 
+{
     public string Name { get; set; }
     public string Type { get; set; }
     public decimal Weight { get; set; }
